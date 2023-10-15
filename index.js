5da4340bb320fc4ac55f348269fd1b8a2d646548
Lester del Rosario <79028758+Lester1105@users.noreply.github.com>
2023-10-15 21:31:16 +0800

    my improved code

--- index.js
+++ index.js
@@ -1,59 +1,87 @@
 let playerSelected = document.querySelector('#playerSelected');
 let notification= document.querySelector("#notification");
 let computerSelected= document.querySelector("#computerSelected");
-//const choiceBtn=document.querySelector('#choiceBtns');
-
-//let compSelect='';
+const choiceBtn=document.querySelectorAll('.choiceBtns');
+let playerScoreCard=document.querySelector(".playerScore");
+let computerScoreCard=document.querySelector(".computerScore");
 let playerScore=0;
 let computerScore = 0;
+let computer="✊";
+let player="✊";
+let result;
+
 function getComputerChoice(){
-let random = Math.floor(Math.random()*3) +1;
-let computerSelect='';
-switch(random){
-    case 1:
-        computerSelect = "rock"
-        break;
-    case 2:
-        computerSelect= "paper"
-        break;
-    case 3:
-        computerSelect="scissor"
-        break;
-}
-return computerSelect;
-};
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection =prompt('please select your weapon! rock > paper > scissor');
-    computerSelection=getComputerChoice();
-
-
-    if(playerSelection==computerSelection){
-        return "draw";
-    }else if(playerSelection=="rock"&& computerSelection=="paper"){
-        computerScore +=1;
-        return "you lose this round";
-    }else if(playerSelection=="paper"&& computerSelection=="scissor"){
-        computerScore +=1;
-        return "you lose this round";
-    }else if(playerSelection=="scissor"&& computerSelection=="rock"){
-        computerScore +=1;
-        return "you lose this round";
-    }else if(playerSelection=="rock"&& computerSelection=="scissor"){
-        playerScore +=1;
-        return "you win this round";
-    }else if(playerSelection=="paper"&& computerSelection=="rock"){
-        playerScore +=1;
-        return "you win this round";
-    }else if(playerSelection=="scissor"&& computerSelection=="paper"){
-        playerScore +=1;
-        return "you win this round";
+    let random = Math.floor(Math.random()*3) +1;
+    
+    switch(random){
+        case 1:
+            computer = "✊"
+            break;
+        case 2:
+            computer= "🖐"
+            break;
+        case 3:
+            computer="✌️"
+            break;
     }
+    return computer;
+    };
+
+
+    
+
+function playRound() {
+    if(player==computer){
+        result= "draw";
+    }else if(player=="✊"&& computer=="🖐"){
+       computerScore+=1;
+        result="you lose this round";
+    }else if(player=="🖐"&& computer=="✌️"){
+       computerScore+=1;
+        result="you lose this round";
+    }else if(player=="✌️"&& computer=="✊"){
+        computerScore+=1;
+        result="you lose this round";
+    }else if(player=="✊"&& computer=="✌️"){
+        playerScore+=1;
+        result="you win this round";
+    }else if(player=="🖐"&& computer=="✊"){
+       playerScore+=1;
+        result="you win this round";
+    }else if(player=="✌️"&& computer=="🖐"){
+       playerScore+=1;
+        result="you win this round";
+    }
+    if(playerScore>=6){
+        result="Congratulations! You've won against a computer, whould you try again?";
+        playerScore=0;
+        computerScore=0;
+    }else if(computerScore>=6){
+        result="Aww! You've beaten by a computer,whould you want to try again?";
+        playerScore=0;
+        computerScore=0;
+    }
+
+   return result;
+
 }
-function game(){
-   while (playerScore<=3 || computerScore<=3){
-    playRound();
-    console.log(playRound());
-   }
+
+choiceBtn.forEach(button =>
+    button.addEventListener('click',()=>{
+        player=button.textContent;
+         getComputerChoice();
+   playerSelected.textContent=player;
+   computerSelected.textContent=getComputerChoice();
+   notification.textContent=playRound();
+   playerScoreCard.textContent=playerScore;
+   computerScoreCard.textContent=computerScore;
+    
+}));
+
+function reset(){
+    playerScore=0;
+    computerScore=0;
+    notification.textContent="Try your luck!!";
+    playerScoreCard.textContent=playerScore;
+    computerScoreCard.textContent=computerScore;
 }
-console.log(game());
